16ab81d9d887599d30f155cb6685f7a7195dd8ef
kaathihan luxmachandran <kaathihan.luxmachandran@ontariotechu.net>
2022-12-05 22:23:01 -0500

    reminder

---
+++ .idea/reminder/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder/view/google_sign_in.dart';
+import 'package:reminder/view/home_page.dart';
+import 'package:reminder/view/login.dart';
+import 'package:reminder/view/reminder_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => GoogleSignInProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: HomePage(),
+      ));
+}
